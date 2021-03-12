2c0f5093bafb81c7cc2becdf10ddd1905db1d984
Théo Maudet <theo.maudet@icloud.com>
2021-03-12 16:26:26 +0100

    'clientlist'

--- App/Views/customer.php
+++ App/Views/customer.php
@@ -1,5 +1,29 @@
-<?php
+<section id="listclient">
 
-foreach ($customers as $customer) {
-  echo $customer->customerName;
-}
+<table style="width:100%">
+  <tr>
+    <th>Pseudo</th>
+    <th>Nom</th>
+    <th>Prénom</th>
+    <th>Phone</th>
+    <th>ville</th>
+    <th>Pays</th>
+  </tr>
+
+  <?php
+  foreach ($customers as $customer) {
+    echo '<tr>';
+    echo '<td>' . $customer->customerName . '</td>';
+    echo '<td>' . $customer->contactLastName . '</td>';
+    echo '<td>' . $customer->contactFirstName . '</td>';
+    echo '<td>' . $customer->phone . '</td>';
+    echo '<td>' . $customer->city . '</td>';
+    echo '<td>' . $customer->state . '</td>';
+    echo '</tr>';
+
+  }
+  ?>
+</table>
+
+
+</section>
