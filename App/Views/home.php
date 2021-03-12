c6d0437f47d415e0fd2c0b0236c2d6cc39b4f051
AuroreFournier <aurore.fournier0809@gmail.com>
2021-03-12 16:16:20 +0100

    img

--- App/Views/home.php
+++ App/Views/home.php
@@ -1,3 +1,13 @@
 <?php
 
+
 echo 'salut!!!!';
+?>
+
+<div class="cars">
+    <a href="#" target="_blank"><img src="../img/img1.png" alt=""></a>
+    <a href="#" target="_blank"><img src="../img/img2.png" alt=""></a>
+    <a href="#" target="_blank"><img src="../img/img3.png" alt=""></a>
+    <a href="#" target="_blank"><img src="../img/img4.png" alt=""></a>
+
+</div>
